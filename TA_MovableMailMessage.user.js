a2652b50d3fa7ebeef56c1876e2c980db7eb2f9e
netquik <netquik@gmail.com>
2020-03-03 03:42:12 +0100

    Aesthetics Fixes

--- TA_MovableMailMessage.user.js
+++ TA_MovableMailMessage.user.js
@@ -36,10 +36,10 @@
             var source = qxA.switchMenuOverlay.toString()
             var oOmember = source.match(/deactivate\(\)\;this\.([A-Za-z_]+)\(/)[1];
             var oOMod = "if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.close();else {if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(false);};if (this.__nN.getLayoutParent() instanceof MMovableMail){this.__nN.getLayoutParent().remove(this.__nN);}this.__np.focus();};if (this.__nN != iF) {this.__nN = iF;if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.open();else {this.__np.add(this.__nN, {left: 0,top: 0});if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(true);};} else this.__nv.reset();};";
-            qx.core.Init.getApplication()[oOmember] = new Function('iF', oOMod);
+            qxA[oOmember] = new Function('iF', oOMod);
             // Mod for centerposition function - should not center what is already centered
             var centerMod = "if (this.getLayoutParent() instanceof MMovableMail === false) {var A=qx.core.Init.getApplication();var C=A.getDesktop().getBounds();var B=A.getMenuBar().getBounds();var z=A.getCurrentBottomOverlay();var x=Math.floor((C.width-webfrontend.gui.MenuOverlayWidget.OverlayWidth)/2);var y=B.height;if(z&&z.isVisible())this.setLayoutProperties({left:x,top:y,bottom:webfrontend.Application.legacySocHeight+webfrontend.gui.notifications.Ticker.TickerHeight});else this.setLayoutProperties({left:x,top:y,bottom:webfrontend.gui.notifications.Ticker.TickerHeight});}";
-            webfrontend.gui.MenuOverlayWidget.prototype.centerPosition = new Function('', centerMod);
+            MOW.centerPosition = new Function('', centerMod);
 
         }
 
