f28591090479188f1e2a931863b8bdfd9c725152
netquik <netquik@gmail.com>
2020-03-09 19:29:28 +0100

    Fix for adding Recipients from field

--- TA_MovableMailMessage.user.js
+++ TA_MovableMailMessage.user.js
@@ -2,13 +2,13 @@
 // @name            MovableMailMessage
 // @description     Make Mail Messages Movable
 // @author          Netquik [SoO] (https://github.com/netquik)
-// @version         1.0.1
+// @version         1.0.2
 // @namespace       https://cncapp*.alliances.commandandconquer.com/*/index.aspx*
 // @include         https://cncapp*.alliances.commandandconquer.com/*/index.aspx*
 // @updateURL       https://raw.githubusercontent.com/netquik/CnCTA-SoO-SCRIPT-PACK/master/TA_MovableMailMessage.user.js
 // ==/UserScript==
 /**
- *  License: CC-BY-NC-SA 3.0
+ *  License: CC-BY-NC-SA 4.0
  */
 (function () {
     var injectFunction = function () {
@@ -60,13 +60,13 @@
 
             var qxA = qx.core.Init.getApplication();
             var MOW = webfrontend.gui.MenuOverlayWidget.prototype;
-            var MOW_open = "var A=qx.core.Init.getApplication();var MM = MMovableMail.getInstance();if(A.getCurrentMenuOverlay()!==this)m=MM.createMM(); this.toggle(); m.add(this,{top:0,left:0});MM.activateMM();m.fadeIn();this.addListener('move', function (e) {this.setLayoutProperties({top:0,left:0})})";
+            var MOW_open = "var A=qx.core.Init.getApplication();var MM = MMovableMail.getInstance();if(A.getCurrentMenuOverlay()!==this){m=MM.createMM(); this.toggle(); m.add(this,{top:0,left:0});MM.activateMM();m.fadeIn();this.addListener('move', function (e) {this.setLayoutProperties({top:0,left:0})})}";
             MOW.open = new Function('', MOW_open);
 
             var source = qxA.switchMenuOverlay.toString()
             var oOmember = source.match(/deactivate\(\)\;this\.([A-Za-z_]+)\(/)[1];
-            var oOMod = "if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.close();else {if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(false);};if (this.__nN.getLayoutParent() instanceof MMovableMail){this.__nN.getLayoutParent().remove(this.__nN);}this.__np.focus();};if (this.__nN != iF) {this.__nN = iF;if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.open();else {this.__np.add(this.__nN, {left: 0,top: 0});if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(true);};} else this.__nv.reset();};";
-            qxA[oOmember] = new Function('iF', oOMod);
+            var oOMod = "if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.close();else {if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(false);};if (this.__nN.getLayoutParent() instanceof MMovableMail && iF){this.__nN.getLayoutParent().exclude();}this.__np.focus();};if (this.__nN != iF) {this.__nN = iF;if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.open();else {this.__np.add(this.__nN, {left: 0,top: 0});if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(true);};} else this.__nv.reset();};";
+            qxA[oOmember] = new Function('iF', oOMod); 
             // Mod for centerposition function - should not center what is already centered
             var centerMod = "if (this.getLayoutParent() instanceof MMovableMail === false) {var A=qx.core.Init.getApplication();var C=A.getDesktop().getBounds();var B=A.getMenuBar().getBounds();var z=A.getCurrentBottomOverlay();var x=Math.floor((C.width-webfrontend.gui.MenuOverlayWidget.OverlayWidth)/2);var y=B.height;if(z&&z.isVisible())this.setLayoutProperties({left:x,top:y,bottom:webfrontend.Application.legacySocHeight+webfrontend.gui.notifications.Ticker.TickerHeight});else this.setLayoutProperties({left:x,top:y,bottom:webfrontend.gui.notifications.Ticker.TickerHeight});}";
             MOW.centerPosition = new Function('', centerMod);
