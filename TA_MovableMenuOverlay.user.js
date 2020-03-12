2cd6e36930163be1cd8f9ccd5d209fa596cfaa16
netquik <netquik@gmail.com>
2020-03-12 20:35:22 +0100

    set more MinHieght

--- TA_MovableMenuOverlay.user.js
+++ TA_MovableMenuOverlay.user.js
@@ -2,7 +2,7 @@
 // @name            MovableMenuOverlay
 // @description     Make Overlay Menu Windows Movable including Forum and Mail
 // @author          Netquik [SoO] (https://github.com/netquik)
-// @version         1.0.3
+// @version         1.0.3b
 // @namespace       https://cncapp*.alliances.commandandconquer.com/*/index.aspx*
 // @include         https://cncapp*.alliances.commandandconquer.com/*/index.aspx*
 // @updateURL       https://raw.githubusercontent.com/netquik/CnCTA-SoO-SCRIPT-PACK/master/TA_MovableMenuOverlay.user.js
@@ -65,7 +65,7 @@
 
             var source = qxA.switchMenuOverlay.toString()
             var oOmember = source.match(/deactivate\(\)\;this\.([A-Za-z_]+)\(/)[1];
-            var oOMod = "if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.close();else {if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(false);};if (this.__nN.getLayoutParent() instanceof MMOverlay){this.__nN.getLayoutParent().exclude();}this.__np.focus();};if (this.__nN != iF) {this.__nN = iF;if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.open();else {var MM = MMOverlay.getInstance(); m=MM.createMM(); m.add(this.__nN, {left: 0,top: 0});MM.activateMM();m.fadeIn();this.__nN.setMaxHeight(700);this.__nN.setMinHeight(500);this.__nN.addListener('move', function (e) {this.setLayoutProperties({top:0,left:0})});this.__nN.addListener('appear', function (e) {this.setLayoutProperties({top:0,left:0})});if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(true);};} else this.__nv.reset();};";
+            var oOMod = "if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.close();else {if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(false);};if (this.__nN.getLayoutParent() instanceof MMOverlay){this.__nN.getLayoutParent().exclude();}this.__np.focus();};if (this.__nN != iF) {this.__nN = iF;if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.open();else {var MM = MMOverlay.getInstance(); m=MM.createMM(); m.add(this.__nN, {left: 0,top: 0});MM.activateMM();m.fadeIn();this.__nN.setMinHeight(650);this.__nN.addListener('move', function (e) {this.setLayoutProperties({top:0,left:0})});this.__nN.addListener('appear', function (e) {this.setLayoutProperties({top:0,left:0})});if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(true);};} else this.__nv.reset();};";
             qxA[oOmember] = new Function('iF', oOMod); 
             // Mod for centerposition function - should not center what is already centered
             var centerMod = "if (this.getLayoutParent() instanceof MMOverlay === false) {var A=qx.core.Init.getApplication();var C=A.getDesktop().getBounds();var B=A.getMenuBar().getBounds();var z=A.getCurrentBottomOverlay();var x=Math.floor((C.width-webfrontend.gui.MenuOverlayWidget.OverlayWidth)/2);var y=B.height;if(z&&z.isVisible())this.setLayoutProperties({left:x,top:y,bottom:webfrontend.Application.legacySocHeight+webfrontend.gui.notifications.Ticker.TickerHeight});else this.setLayoutProperties({left:x,top:y,bottom:webfrontend.gui.notifications.Ticker.TickerHeight});}";
