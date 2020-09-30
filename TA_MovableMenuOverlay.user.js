1ac9e23e374e376268f24b62fce5a45c59642267
netquik <netquik@gmail.com>
2020-09-30 23:47:48 +0200

    Pste Coords FIX1

--- TA_MovableMenuOverlay.user.js
+++ TA_MovableMenuOverlay.user.js
@@ -2,7 +2,7 @@
 // @name            MovableMenuOverlay
 // @description     Make Overlay Menu Windows Movable including Forum and Mail
 // @author          Netquik [SoO] (https://github.com/netquik)
-// @version         1.0.3b
+// @version         1.0.4
 // @namespace       https://cncapp*.alliances.commandandconquer.com/*/index.aspx*
 // @include         https://cncapp*.alliances.commandandconquer.com/*/index.aspx*
 // @updateURL       https://raw.githubusercontent.com/netquik/CnCTA-SoO-SCRIPT-PACK/master/TA_MovableMenuOverlay.user.js
@@ -10,6 +10,14 @@
 /**
  *  License: CC-BY-NC-SA 4.0
  */
+
+/* 
+codes by NetquiK
+----------------
+- Original code
+- Bug Fix for Paste Coords
+----------------
+*/
 (function () {
     var injectFunction = function () {
         function ModMailMessage() {
@@ -61,20 +69,16 @@
             var qxA = qx.core.Init.getApplication();
             var MOW = webfrontend.gui.MenuOverlayWidget.prototype;
             /* var MOW_open = "var A=qx.core.Init.getApplication();var MM = MMovableMail.getInstance();if(A.getCurrentMenuOverlay()!==this){m=MM.createMM(); this.toggle(); m.add(this,{top:0,left:0});MM.activateMM();m.fadeIn();this.addListener('move', function (e) {this.setLayoutProperties({top:0,left:0})})}";
-            MOW.open = new Function('', MOW_open); */ 
-
-            var source = qxA.switchMenuOverlay.toString()
+            MOW.open = new Function('', MOW_open); */
+            var source = qxA.switchMenuOverlay.toString();
             var oOmember = source.match(/deactivate\(\)\;this\.([A-Za-z_]+)\(/)[1];
-            var oOMod = "if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.close();else {if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(false);};if (this.__nN.getLayoutParent() instanceof MMOverlay){this.__nN.getLayoutParent().exclude();}this.__np.focus();};if (this.__nN != iF) {this.__nN = iF;if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.open();else {var MM = MMOverlay.getInstance(); m=MM.createMM(); m.add(this.__nN, {left: 0,top: 0});MM.activateMM();m.fadeIn();this.__nN.setMinHeight(650);this.__nN.addListener('move', function (e) {this.setLayoutProperties({top:0,left:0})});this.__nN.addListener('appear', function (e) {this.setLayoutProperties({top:0,left:0})});if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(true);};} else this.__nv.reset();};";
-            qxA[oOmember] = new Function('iF', oOMod); 
+            var oOMod = "if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.close();else {if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(false);};if (this.__nN.getLayoutParent() instanceof MMOverlay){this.__nN._deactivate();this.__nN.getLayoutParent().exclude();}this.__np.focus();};if (this.__nN != iF) {this.__nN = iF;if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.open();else {var MM = MMOverlay.getInstance(); m=MM.createMM(); m.add(this.__nN, {left: 0,top: 0});MM.activateMM();m.fadeIn(250);this.__nN.setMinHeight(625);this.__nN.addListener('move', function (e) {this.setLayoutProperties({top:0,left:0})});this.__nN.addListener('appear', function (e) {this.setLayoutProperties({top:0,left:0})});if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(true);};} else this.__nv.reset();};";
+            qxA[oOmember] = new Function('iF', oOMod);
             // Mod for centerposition function - should not center what is already centered
             var centerMod = "if (this.getLayoutParent() instanceof MMOverlay === false) {var A=qx.core.Init.getApplication();var C=A.getDesktop().getBounds();var B=A.getMenuBar().getBounds();var z=A.getCurrentBottomOverlay();var x=Math.floor((C.width-webfrontend.gui.MenuOverlayWidget.OverlayWidth)/2);var y=B.height;if(z&&z.isVisible())this.setLayoutProperties({left:x,top:y,bottom:webfrontend.Application.legacySocHeight+webfrontend.gui.notifications.Ticker.TickerHeight});else this.setLayoutProperties({left:x,top:y,bottom:webfrontend.gui.notifications.Ticker.TickerHeight});}";
             MOW.centerPosition = new Function('', centerMod);
 
         }
-
-
-
         function waitForGame() {
             try {
                 if (typeof qx !== "undefined" && typeof qx.core !== "undefined" && typeof qx.core.Init !== "undefined" && typeof typeof webfrontend.gui !== "undefined" && typeof phe !== "undefined") {
