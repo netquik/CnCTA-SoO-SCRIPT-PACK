0b24a4b182f0afcb7244e0ff392223235d49b670
netquik <netquik@gmail.com>
2020-10-02 19:04:37 +0200

    Rewrite of functions

--- TA_MovableMenuOverlay.user.js
+++ TA_MovableMenuOverlay.user.js
@@ -18,9 +18,10 @@ codes by NetquiK
 - Bug Fix for Paste Coords
 ----------------
 */
+
 (function () {
     var injectFunction = function () {
-        function ModMailMessage() {
+        function ModMenuOverlay() {
             qx.Class.define("MMOverlay", {
                 type: "singleton",
                 extend: qx.ui.container.Composite,
@@ -60,32 +61,28 @@ codes by NetquiK
                             this.MMO._activateMoveHandle(this.MMO.getChildren()[0].getChildren()[13]);
                         }
                     }
-
-
                 }
             });
-
-
             var qxA = qx.core.Init.getApplication();
             var MOW = webfrontend.gui.MenuOverlayWidget.prototype;
-            /* var MOW_open = "var A=qx.core.Init.getApplication();var MM = MMovableMail.getInstance();if(A.getCurrentMenuOverlay()!==this){m=MM.createMM(); this.toggle(); m.add(this,{top:0,left:0});MM.activateMM();m.fadeIn();this.addListener('move', function (e) {this.setLayoutProperties({top:0,left:0})})}";
-            MOW.open = new Function('', MOW_open); */
-            var source = qxA.switchMenuOverlay.toString();
-            var oOmember = source.match(/deactivate\(\)\;this\.([A-Za-z_]+)\(/)[1];
-            var oOMod = "if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.close();else {if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(false);};if (this.__nN.getLayoutParent() instanceof MMOverlay){this.__nN._deactivate();this.__nN.getLayoutParent().exclude();}this.__np.focus();};if (this.__nN != iF) {this.__nN = iF;if (this.__nN) {if (this.__nN instanceof webfrontend.gui.OverlayWindow) this.__nN.open();else {var MM = MMOverlay.getInstance(); m=MM.createMM(); m.add(this.__nN, {left: 0,top: 0});MM.activateMM();m.fadeIn(250);this.__nN.setMinHeight(625);this.__nN.addListener('move', function (e) {this.setLayoutProperties({top:0,left:0})});this.__nN.addListener('appear', function (e) {this.setLayoutProperties({top:0,left:0})});if (this.__nN instanceof webfrontend.gui.MenuOverlayWidget) this.__nN.setActive(true);};} else this.__nv.reset();};";
-            qxA[oOmember] = new Function('iF', oOMod);
-            // Mod for centerposition function - should not center what is already centered
-            var centerMod = "if (this.getLayoutParent() instanceof MMOverlay === false) {var A=qx.core.Init.getApplication();var C=A.getDesktop().getBounds();var B=A.getMenuBar().getBounds();var z=A.getCurrentBottomOverlay();var x=Math.floor((C.width-webfrontend.gui.MenuOverlayWidget.OverlayWidth)/2);var y=B.height;if(z&&z.isVisible())this.setLayoutProperties({left:x,top:y,bottom:webfrontend.Application.legacySocHeight+webfrontend.gui.notifications.Ticker.TickerHeight});else this.setLayoutProperties({left:x,top:y,bottom:webfrontend.gui.notifications.Ticker.TickerHeight});}";
+            var oOMethod = qxA.switchMenuOverlay.toString().match(/deactivate\(\)\;this\.([A-Za-z_]+)\(/)[1];
+            var source = qxA[oOMethod].toString();
+            // MOD injecting MMOverlay in switchMenuOverlay->oO
+            var oOArg = source.match(/function\(([a-zA-Z]+)\)/)[1];
+            var oOMod = source.replace(/function\([a-zA-Z]+\){(if.+setActive\(false\);}})(this\.[_a-zA-Z]+\.focus.+open\(\);}else{).+\.(add.+0}\);)(.+)}/, '$1if(this.__nN.getLayoutParent()instanceof MMOverlay){var t=MMOverlay.getInstance().MMO;this.__nN._deactivate();-1!=t.indexOf(this.__nN)&&t.remove(this.__nN);t.exclude();}$2var MM=MMOverlay.getInstance();m=MM.createMM();m.$3MM.activateMM();m.fadeIn(250);this.__nN.setMinHeight(625);this.__nN.addListener("move",function(e){this.setLayoutProperties({top:0,left:0})});this.__nN.addListener("appear",function(e){this.setLayoutProperties({top:0,left:0})});$4');
+            qxA[oOMethod] = new Function(oOArg, oOMod);
+            // MOD for centerposition function - should not center what is already centered
+            var centerMod = MOW.centerPosition.toString().replace(/function\(\){(var.+;})}/, 'if (this.getLayoutParent() instanceof MMOverlay === false){$1}');
             MOW.centerPosition = new Function('', centerMod);
-
         }
+
         function waitForGame() {
             try {
                 if (typeof qx !== "undefined" && typeof qx.core !== "undefined" && typeof qx.core.Init !== "undefined" && typeof typeof webfrontend.gui !== "undefined" && typeof phe !== "undefined") {
                     var app = qx.core.Init.getApplication();
                     if (app.initDone === true) {
                         try {
-                            ModMailMessage();
+                            ModMenuOverlay();
                             console.time("loaded in");
                             console.group("SoO - MovableMenuOverlay");
                             console.timeEnd("loaded in");
